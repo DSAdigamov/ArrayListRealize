a6017daa5f7934c770781de5b871863dca277552
DSAdigamov <101574437+DSAdigamov@users.noreply.github.com>
2022-07-28 09:27:26 +0300

    start

---
+++ src/main/java/com/example/arraylistrlz/ArrayListRealizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.arraylistrlz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrayListRealizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrayListRealizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arraylistrlz/ArrayListRealizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arraylistrlz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrayListRealizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
